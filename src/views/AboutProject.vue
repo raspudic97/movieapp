079bd882d32b1c2be63780f3bfac73eb5cf93b8d
raspudic97 <raspudic88@gmail.com>
2021-06-19 03:04:17 +0200

    Finalne promjene

--- src/views/AboutProject.vue
+++ src/views/AboutProject.vue
@@ -1,65 +1,66 @@
 <template>
   <div class="aboutProject">
-      <h1>Movie App</h1>
+    <div class="backgroundFilter"></div>
+    <h1>Movie App</h1>
 
-      <p class="about">
-         Movie App je jednostavan projekt za pretraživanje filmova. Na početnoj stranici ćete svaki put kada uđete dobiti nasumično odabran film.
-         Također na stranici možete dobiti listu popularnih i najbolje ocjenjenih filmova. Na stranici "search" moguće je i pretraživati filmove.
-         Kada kliknete na karticu od filma otvorit će se detaljniji opis istog. Na stranici sa detaljnijim opisom imate mogućnost i za pronalaženje 
-         filmova sličnih odabranom filmu (opcija - Find similar movies).
-
-         Ukoliko želite na stranici "feedback" možete ostaviti i komentar koji će se spremiti u lokalnu bazu podataka.
-      </p>
-
-      <h1 class="upute">Šta je potrebno za pokretanje projekta ?</h1>
-
-      <span>1. Dodavanje node_modules foldera</span>
-      <p>Nakon skidanja projekta sa github-a potrebno je u terminalu upisati <strong>"npm install"</strong> da bi se instalirao node_modules folder</p>
-
-      <span>2. Dodavanje axiosa</span>
-      <p>Nakon instalacije node_modules, morate dodati axios tako što ćete u terminalu napisati <strong> "npm install --save axios vue-axios"</strong></p>
-
-      <span>3. Instalacija json-servera</span>
-      <p>Da bi feedback stranica bila funkcionalna potrebno je dodati json-server tako što ćete u terminalu napisati <strong>"npm install json-server -g"</strong></p>
-
-      <span>4. Pokretanje projekta</span>
-      <p>Da bi pokrenuli projekt potrebno je uraditi 2 stvari: <br><br>
-           1. Pokrenuti json-server za lokalnu bazu podataka tako što u terminalu pišete <strong> "json-server db.json" </strong><br><br>
-
-           2. Pokrenuti projekt tako što u terminalu pišete <strong> "npm run serve" </strong><br>
-              - Ctrl + lijevi klik na link će otvoriti projekt u zadanom web browseru
-      </p>
+    <p class="about">
+      Movie App je jednostavna web stranica za pretraživanje filmova. Na
+      početnoj stranici ćete svaki put kada uđete dobiti nasumično odabran film.
+      Također na Movie App-u možete dobiti listu popularnih i najbolje
+      ocjenjenih filmova. Na stranici "search" moguće je i pretraživati filmove
+      po naslovi. Kada kliknete na karticu od filma otvorit će se detaljniji
+      opis istog, a na stranici sa detaljnijim opisom imate mogućnost i za
+      pronalaženje filmova sličnih odabranom filmu (opcija - Find similar
+      movies).
+    </p>
   </div>
 </template>
 
 <style scoped>
+.backgroundFilter {
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100vw;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+}
 
 .aboutProject {
-    padding: 2rem 4rem;
-    max-width: 80vw;
-    margin: 0 auto;
+  padding: 2rem 4rem;
+  width: 100%;
+  height: 100vh;
+  margin: 0 auto;
+  color: white;
+  background: url("https://wallpaperaccess.com/full/752715.jpg");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 span {
-    color: #333;
-    margin: 1rem;
-    font-weight: bold;
-    font-size: 1rem;
-    display: block;
-    text-align: center;
-}
-
-p {
-    text-align: center;
+  color: #333;
+  margin: 1rem;
+  font-weight: bold;
+  font-size: 1rem;
+  display: block;
+  text-align: center;
 }
 
 h1 {
-    text-align: center;
-    padding-bottom: 1rem;
+  text-align: center;
+  padding-bottom: 1rem;
+}
+
+h1,
+p {
+  z-index: 2;
 }
 
 .about {
-    text-align: center;
-    padding: 0;
+  text-align: center;
+  padding: 0;
 }
 </style>
--- src/views/Feedback.vue
+++ src/views/Feedback.vue
@@ -1,34 +1,20 @@
 <template>
   <div class="recenzije">
     <v-container>
-      <v-text-field label="Username" v-model="feedbackUsername"></v-text-field>
-      <v-text-field label="Title" v-model="feedbackTitle"></v-text-field>
+      <v-text-field label="Username"></v-text-field>
+      <v-text-field label="Title"></v-text-field>
 
       <v-textarea
         outlined
         name="input-7-4"
         label="Feedback"
-        v-model="feedback"
-        :value="feedback"
       ></v-textarea>
 
-      <v-btn @click="postFeedback">Post your feedback</v-btn>
+      <v-btn @click="alert">Post your feedback</v-btn>
 
-      <v-alert type="success" class="my-4" v-model="successAlert" dismissible>
+      <v-alert v-if="successAlert" type="success" class="my-4" dismissible>
         Successfuly saved.
       </v-alert>
-
-      <div class="feedbackCard" v-for="feed in feedbacks" :key="feed.id">
-        <v-card class="my-4" color="#333" dark>
-          <v-card-title class="text-h7">
-            {{ feed.feedback_title }}
-          </v-card-title>
-
-          <v-card-subtitle> {{ feed.feedback_content }} </v-card-subtitle>
-
-          <v-card-subtitle> By: {{ feed.username }} </v-card-subtitle>
-        </v-card>
-      </div>
     </v-container>
   </div>
 </template>
@@ -37,41 +23,18 @@
 export default {
   data() {
     return {
-      feedback: "",
-      feedbackUsername: "",
-      feedbackTitle: "",
-      feedbacks: [],
       successAlert: false,
     };
   },
   methods: {
-    getData() {
-      this.axios
-        .get(`http://localhost:3000/feedbacks`)
-        .then((response) => {
-          this.feedbacks = response.data;
-        });
-    },
-    postFeedback() {
-      let newFeedback = {
-        id: parseInt(this.feedbacks.length) + 1,
-        username: this.feedbackUsername,
-        feedback_content: this.feedback,
-        feedback_title: this.feedbackTitle,
-      };
-
-      this.axios.post("http://localhost:3000/feedbacks", newFeedback);
-
+    alert: function() {
       this.successAlert = true;
 
-      setTimeout(() => { 
-        location.reload();
-      }, 5000);
-    },
-  },
-  created() {
-    this.getData();
-  },
+      setTimeout(() => {
+        this.successAlert = false;
+      }, 3000);
+    }
+  }
 };
 </script>
 
